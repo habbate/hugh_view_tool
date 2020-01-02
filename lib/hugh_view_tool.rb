6af9733cf5a99bf7f624084baf5b8142d59cb676
habbate <habbate@edrans.com>
2020-01-02 09:29:32 -0300

    Dev tool initial commit

---
+++ lib/hugh_view_tool.rb
@@ -0,0 +1,7 @@
+require "hugh_view_tool/version"
+require "hugh_view_tool/renderer"
+
+module HughViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hugh_view_tool/version.rb
@@ -0,0 +1,3 @@
+module HughViewTool
+  VERSION = "0.1.0"
+end
